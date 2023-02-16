4674509ca39c63db244981b1f2abe275ab4bd0a8
YuBeen-Park <park_ubin@naver.com>
2023-02-16 14:45:14 +0900

    [feat] 홈 뷰 bottomNavigation 구현

--- app/src/main/java/com/android/shoppi/presentation/main/MainActivity.kt
+++ app/src/main/java/com/android/shoppi/presentation/main/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.android.shoppi.presentation.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil.setContentView
 import com.android.shoppi.R
 import com.android.shoppi.databinding.ActivityMainBinding
 import com.android.shoppi.util.binding.BindingActivity
@@ -10,5 +8,10 @@ import com.android.shoppi.util.binding.BindingActivity
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initLayout()
+    }
+
+    private fun initLayout() {
+        binding.bnvMain.itemIconTintList = null
     }
 }

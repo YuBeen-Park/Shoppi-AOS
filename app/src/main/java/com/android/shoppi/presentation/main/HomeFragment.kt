2b7bc87a4a7aee89df2e0a187e2fb9c4d302fcb9
YuBeen-Park <park_ubin@naver.com>
2023-02-16 16:21:06 +0900

    [feat] 홈 뷰 Navigation Component 이용해서 화면 이동 구현

--- app/src/main/java/com/android/shoppi/presentation/main/HomeFragment.kt
+++ app/src/main/java/com/android/shoppi/presentation/main/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.android.shoppi.presentation.main
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.android.shoppi.R
 import com.android.shoppi.databinding.FragmentHomeBinding
 import com.android.shoppi.util.binding.BindingFragment
@@ -16,9 +17,12 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
         binding.btnEnterProductDetail.setOnClickListener {
             //parentFragmentManager
             //childFragmentManager
-            parentFragmentManager.beginTransaction() // fragment의 추가 삭제 교체 요청
-                .add(R.id.container_main, ProductDetailFragment())
-                .commit()
+//            parentFragmentManager.beginTransaction() // fragment의 추가 삭제 교체 요청
+//                .add(R.id.container_main, ProductDetailFragment())
+//                .commit()
+            // 위의 방식을 이용해서 구현했을 때 기존의 fragment가 뒤에 겹쳐서 보임
+            // -> 위에 다른 fragment을 올리기 때문?
+            findNavController().navigate(R.id.action_home_to_product_detail)
         }
     }
 }
--- app/src/main/java/com/android/shoppi/presentation/main/MainActivity.kt
+++ app/src/main/java/com/android/shoppi/presentation/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.android.shoppi.presentation.main
 
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.android.shoppi.R
 import com.android.shoppi.databinding.ActivityMainBinding
 import com.android.shoppi.util.binding.BindingActivity
@@ -13,5 +15,10 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
 
     private fun initLayout() {
         binding.bnvMain.itemIconTintList = null
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            binding.bnvMain.setupWithNavController(it)
+        }
     }
 }

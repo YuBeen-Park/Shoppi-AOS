3d37caeddd54dcf2c60170c20ee7ae2ff08c712b
YuBeen-Park <park_ubin@naver.com>
2023-03-01 23:23:10 +0900

    [feat] Lab2

--- app/src/main/java/com/android/shoppi/presentation/main/HomeFragment.kt
+++ app/src/main/java/com/android/shoppi/presentation/main/HomeFragment.kt
@@ -2,12 +2,13 @@ package com.android.shoppi.presentation.main
 
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.fragment.findNavController
 import com.android.shoppi.R
 import com.android.shoppi.databinding.FragmentHomeBinding
 import com.android.shoppi.util.AssetLoader
 import com.android.shoppi.util.binding.BindingFragment
-import org.json.JSONObject
+import com.android.shoppi.util.setImage
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.gson.Gson
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -16,32 +17,45 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
         initLayout()
     }
 
-    private fun initLayout(){
+    private fun initLayout() {
         val assetLoader = AssetLoader()
-        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
-        if(!homeData.isNullOrEmpty()){
-            val jsonObject = JSONObject(homeData)
-            val title = jsonObject.getJSONObject("title")
-            val text = title.getString("text")
-            val iconUrl = title.getString("icon_url")
+        val homeJsonString = assetLoader.getJsonString(requireContext(), "home.json")
+        if (!homeJsonString.isNullOrEmpty()) {
+            val gson = Gson()
+            val homeData = gson.fromJson(homeJsonString, HomeData::class.java)
 
-            val topBanners = jsonObject.getJSONArray("top_banners")
-            val firstBanner = topBanners.getJSONObject(0)
-            firstBanner.getString("label")
+            binding.tvToolbarHomeTitle.text = homeData.title.text
+            binding.ivToolbarHomeIcon.setImage(homeData.title.iconUrl)
+
+            binding.vpHomeBanner.adapter = HomeBannerAdapter().apply {
+                submitList(homeData.topBanners)
+            }
+            binding.vpHomeBanner.offscreenPageLimit = 3
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+            val screenWidth = resources.displayMetrics.widthPixels
+            val offset = screenWidth - pageWidth - pageMargin
+            binding.vpHomeBanner.setPageTransformer { page, position ->
+                page.translationX = position * -offset
+            }
+            TabLayoutMediator(
+                binding.vpHomeBannerIndicator, binding.vpHomeBanner
+            ) { tab, position ->
+
+            }.attach()
         }
-
     }
 
     private fun addListeners() {
-        binding.btnEnterProductDetail.setOnClickListener {
-            //parentFragmentManager
-            //childFragmentManager
-//            parentFragmentManager.beginTransaction() // fragment의 추가 삭제 교체 요청
-//                .add(R.id.container_main, ProductDetailFragment())
-//                .commit()
-            // 위의 방식을 이용해서 구현했을 때 기존의 fragment가 뒤에 겹쳐서 보임
-            // -> 위에 다른 fragment을 올리기 때문?
-            findNavController().navigate(R.id.action_home_to_product_detail)
-        }
+//        binding.btnEnterProductDetail.setOnClickListener {
+//            //parentFragmentManager
+//            //childFragmentManager
+////            parentFragmentManager.beginTransaction() // fragment의 추가 삭제 교체 요청
+////                .add(R.id.container_main, ProductDetailFragment())
+////                .commit()
+//            // 위의 방식을 이용해서 구현했을 때 기존의 fragment가 뒤에 겹쳐서 보임
+//            // -> 위에 다른 fragment을 올리기 때문?
+//            findNavController().navigate(R.id.action_home_to_product_detail)
+//        }
     }
 }

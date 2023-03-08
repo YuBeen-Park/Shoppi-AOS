ff263280ecd64f8716417b63788ac676e6ec74d5
YuBeen-Park <park_ubin@naver.com>
2023-03-08 23:39:35 +0900

    [feat] 뷰모델, repository, datasource 적용

---
+++ app/src/main/java/com/android/shoppi/presentation/home/HomeViewModel.kt
@@ -0,0 +1,27 @@
+package com.android.shoppi.presentation.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.shoppi.presentation.main.Banner
+import com.android.shoppi.presentation.main.Title
+import com.android.shoppi.repository.HomeRepository
+
+class HomeViewModel(private val homeRepository: HomeRepository) : ViewModel() {
+    private val _title = MutableLiveData<Title>()
+    val title: LiveData<Title> get() = _title
+    private val _topBanners = MutableLiveData<List<Banner>>()
+    val topBanners: LiveData<List<Banner>> get() = _topBanners
+
+    init {
+        fetchHomeData()
+    }
+
+    private fun fetchHomeData() {
+        val homeData = homeRepository.getHomeData()
+        homeData?.let { data ->
+            _title.value = data.title
+            _topBanners.value = data.topBanners
+        }
+    }
+}

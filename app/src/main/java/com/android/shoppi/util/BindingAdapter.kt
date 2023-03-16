b08c500618e01b954c86da0b5114e7df6b95fad8
YuBeen-Park <park_ubin@naver.com>
2023-03-16 22:41:20 +0900

    [feat] 카테고리 상세뷰 인기상품 ui 구현

--- app/src/main/java/com/android/shoppi/util/BindingAdapter.kt
+++ app/src/main/java/com/android/shoppi/util/BindingAdapter.kt
@@ -1,9 +1,11 @@
 package com.android.shoppi.util
 
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import coil.load
+import coil.transform.CircleCropTransformation
 import com.android.shoppi.R
 import java.text.DecimalFormat
 import kotlin.math.roundToInt
@@ -11,6 +13,13 @@ import kotlin.math.roundToInt
 @BindingAdapter("image")
 fun ImageView.setImage(imageUrl: String) {
     this.load(imageUrl)
+}
+
+@BindingAdapter("imageCircle")
+fun ImageView.setCircleImage(imageUrl: String){
+    this.load(imageUrl) {
+        transformations(CircleCropTransformation())
+    }
 }
 
 @BindingAdapter("priceAmount")
@@ -23,4 +32,14 @@ fun applyPriceFormat(view: TextView, price: Int) {
 fun applyPriceDiscountRate(view: TextView, price: Int, discountRate: Int) {
     val discountPrice = (((100 - discountRate) / 100.0) * price).roundToInt()
     applyPriceFormat(view, discountPrice)
+}
+
+@BindingAdapter("isVisible")
+fun TextView.setVisibility(isVisible: Boolean){
+    if(isVisible){
+        this.visibility = View.VISIBLE
+    }
+    else{
+        this.visibility = View.GONE
+    }
 }

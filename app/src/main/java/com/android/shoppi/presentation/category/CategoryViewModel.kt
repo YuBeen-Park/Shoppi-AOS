4b326ddbb4a02222a46e3c3cada13b524bfaa121
YuBeen-Park <park_ubin@naver.com>
2023-03-16 20:45:58 +0900

    [feat] CategoryDetail뷰 생성, category->category-detail 화면 이동 구현

--- app/src/main/java/com/android/shoppi/presentation/category/CategoryViewModel.kt
+++ app/src/main/java/com/android/shoppi/presentation/category/CategoryViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.android.shoppi.util.Event
 import kotlinx.coroutines.launch
 
 class CategoryViewModel(
@@ -12,12 +13,19 @@ class CategoryViewModel(
 
     private val _items = MutableLiveData<List<Category>>()
     val items: LiveData<List<Category>> get() = _items
+    private val _openCategoryEvent = MutableLiveData<Event<Category>>()
+    val openCategoryEvent: LiveData<Event<Category>> get() = _openCategoryEvent
+    //event를 쓰는 이유 라이브데이터를 한번만 사용하고 싶을때? 한번 확인하는 용으로 쓰고 싶을대인듯
 
     init {
         loadCategory()
     }
 
-    fun loadCategory() {
+    fun openCategoryDetail(category: Category) {
+        _openCategoryEvent.value = Event(category)
+    }
+
+    private fun loadCategory() {
         viewModelScope.launch {
             val categories = categoryRepository.getCategories()
             _items.value = categories
---
+++ app/src/main/java/com/android/shoppi/presentation/categoryDetail/CategoryDetailFragment.kt
@@ -0,0 +1,23 @@
+package com.android.shoppi.presentation.categoryDetail
+
+import android.os.Bundle
+import android.view.View
+import com.android.shoppi.R
+import com.android.shoppi.databinding.FragmentCategoryDetailBinding
+import com.android.shoppi.util.binding.BindingFragment
+
+class CategoryDetailFragment :
+    BindingFragment<FragmentCategoryDetailBinding>(R.layout.fragment_category_detail) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        val categoryId = requireArguments().getString(KEY_CATEGORY_ID)
+        val categoryLabel = requireArguments().getString(KEY_CATEGORY_LABEL)
+        binding.toolbarCategoryDetail.title = categoryLabel
+    }
+
+    companion object {
+        const val KEY_CATEGORY_ID = "category_id"
+        const val KEY_CATEGORY_LABEL = "category_label"
+    }
+}
